c5441512f46fd695f3a738e793ed324d19feab67
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-03-27 16:55:59 -0500

    getting the server going real quick

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-
+const port = 3000;
+const express = require('express')
+const app = express()
+const pokemon = require('./models/pokemon.js')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 
 
