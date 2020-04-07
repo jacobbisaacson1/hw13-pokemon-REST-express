a65a3ac6717c36d34e5bf679f9c9fa45c8e3a1f5
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-06 19:28:09 -0500

    done

--- server.js
+++ server.js
@@ -1,90 +1,74 @@
-
-
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const methodOverride = require("method-override")
 const pokemon = require('./models/pokemon.js')
+const methodOverride = require('method-override')
 
-
-//midware
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
-app.use ((req, res, next) => {
-  console.log("running through middleware")
-  next()
-}) 
 
-// get
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', {pokemon: pokemon})
+    // res.send(pokemon)
+})
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', {
-        pokemon: pokemon
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs', {pokemon: pokemon})
+})
+
+
+app.get('/pokemon/:id', (req, res) => {
+    const pokemonIndex = pokemon[req.params.id]
+
+    res.render('show.ejs', {pokemonIndex: pokemonIndex})
+})
+
+
+app.get('/pokemon/:id/edit', (req, res) => {
+    //const pokemonToEdit = pokemon[req.params.id]
+    res.render('edit.ejs', {
+        pokemon: pokemon[req.params.id],
+        index: req.params.id
     })
 })
 
 
+app.post('/pokemon', (req, res) => {
+    const pokemonToAdd = {
+        name: req.body.name,
+        img: req.body.image
+    }
 
-// app.get("/pokemon/:id", (req, res) => {
-//     res.render("show.ejs", {
-//         thisPokemon: pokemon[req.params.id],
-//         indexOfPokemonToDelete: req.params.id
-//     })
+    pokemon.push(pokemonToAdd)
 
-// })
+    res.redirect('/pokemon')
+})
 
-// app.post("/pokemon", (req, res) => {
-//     console.log(req.body)
-//     const pokemonToAdd = {
-//         name: req.body.name,
-//         img: req.body.img
-//     }
-//     pokemon.push(pokemonToAdd)
-//     res.redirect('/pokemon')
-// })
+app.delete('/pokemon/:id', (req, res) => {
+    const indexOfPokemonToDelete = req.params.id
+    const pokemonIndex = pokemon[req.params.id]
+    console.log(`This is indexOfPokemonToDelete: ${indexOfPokemonToDelete}`)
+    pokemon.splice(indexOfPokemonToDelete, 1)
+    res.redirect('/pokemon')
+})
 
+app.put('/pokemon/:id/', (req, res) => {
+    const updatedPokemon = {
+        name: req.body.name,
+        img: req.body.image
+    }
 
-// app.delete('/pokemon/:id', (req, res) => {
-//     const indexOfPokemonToDelete = req.params.id
-//     pokemon.splice(indexOfPokemonToDelete, 1)
-//     res.redirect('/pokemon')
-// })
+    pokemon[req.params.id] = updatedPokemon
 
-// app.get('/pokemon/:id/edit', (req, res) => {
-
-//     const pokemonToEdit = pokemon[req.params.id]
-//     res.render('edit.ejs', {
-//         pokemon: pokemonToEdit,
-//         indexOfPokemonToEdit: req.params.id
-//     })
-// })
-
-// app.put('/pokemon/:id', (req, res) => {
-//     const edittedPokemon = {
-//         name: req.body.name,
-//         img: req.body.img
-//     }
-//     pokemon[req.params.id] = edittedPokemon
-//     res.redirect('/pokemon')
-
-// })
-
-
-
-
-
-
-
-
-
+    res.redirect('/pokemon')
+})
 
 
 
 
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000")
+    console.log('Server is running on 3000')
 })
-
